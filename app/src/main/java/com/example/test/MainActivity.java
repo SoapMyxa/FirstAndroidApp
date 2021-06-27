135b47a73b73ac25d3abb4fdbe3585df2e43f030
SoapMyxa <glebban@mail.ru>
2021-06-27 16:56:43 +0600

    Initial commit

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+      //  Button button = findViewById(R.id.button1);
+
+     //   button.setText("asd");
+
+
+    }
+
+}
