4f825938c73af2d1ec544ffe165c78adc8a6abb3
SoapMyxa <glebban@mail.ru>
2021-07-05 02:44:48 +0600

    Task 5. Added custom view

---
+++ app/src/main/java/com/example/test/activities/CustomViewActivity.java
@@ -0,0 +1,19 @@
+package com.example.test.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.test.R;
+
+public class CustomViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_view);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/test/views/RectangleView.java
@@ -0,0 +1,81 @@
+package com.example.test.views;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class RectangleView extends View {
+
+
+    Paint paint = new Paint();
+
+    public RectangleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        int x = getWidth() /2;
+        int y = getHeight() /2;
+
+        paint.setColor(Color.MAGENTA);
+        canvas.drawRect(x-100, y-100, x+100, y+100, paint);
+        super.onDraw(canvas);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int widthMode = View.MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = View.MeasureSpec.getSize(widthMeasureSpec);
+
+        int heightMode = View.MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = View.MeasureSpec.getSize(heightMeasureSpec);
+
+        int width;
+        int height;
+        int desiredWidth = 400;
+        int desiredHeight = 400;
+
+        /**
+         * EXACTLY - если в layout_width(height) указали размер в dp(sp итд)
+         * AT_MOST - если указали match_parent
+         * UNSPECIFIED - если указали wrap_content
+         */
+
+        if (widthMode == View.MeasureSpec.EXACTLY) {
+            width = widthSize;
+        } else if (widthMode == View.MeasureSpec.AT_MOST) {
+            width = Math.min(desiredWidth, widthSize);
+        } else {
+            width = desiredWidth;
+        }
+
+        if (heightMode == View.MeasureSpec.EXACTLY) {
+            height = heightSize;
+        } else if (heightMode == View.MeasureSpec.AT_MOST) {
+            height = Math.min(desiredHeight, heightSize);
+        } else {
+            height = desiredHeight;
+        }
+
+        if (width < 0) {
+            width = 0;
+        }
+
+        if (height < 0) {
+            height = 0;
+        }
+
+        this.setMeasuredDimension(width, height);
+
+    }
+
+}
